c932af4d14c92cc3e4c2f0bf9c0dfe1c65aef67c
FernandaBuck <0239793@up.edu.mx>
2024-08-20 18:51:59 -0600

    Add files via upload

---
+++ Mi_cv.py
@@ -0,0 +1,65 @@
+import streamlit as st
+
+# Título del CV
+st.title("Curriculum Vitae Fernanda Buck")
+
+# Información personal
+st.header("Información Personal")
+st.markdown("""
+- **Nombre:** María Fernanda Buck Nuñez
+- **Correo Electrónico:** fernandabuckn@outlook.com
+- **Teléfono:** 477 648 7258
+""")
+
+# Resumen profesional
+st.header("Resumen Profesional")
+st.markdown("""
+Actualmente, me encuentro en el octavo semestre de la carrera de Administración y Finanzas en la Universidad Panamericana, campus Guadalajara. Soy una persona altamente organizada y proactiva, siempre comprometida a dar lo mejor de mí en todas mis actividades. Disfruto trabajar en equipo y considero que la colaboración es clave para alcanzar metas exitosas. Estoy en búsqueda de desafíos profesionales que me permitan crecer y superarme.
+""")
+
+# Experiencia profesional
+st.header("Experiencia Profesional")
+st.markdown("""
+### Intern en Contabilidad y Administración
+**[Wilson Abogados S.C] - Abril 2021 - Agosto 2022**
+- Apoyo en registro contable y administrativo de operaciones propias del despacho y de clientes externos.
+- Apoyo en el desarrollo de modelos de análisis propios y para clientes.
+- Contabilidad y Finanzas.
+
+### Presidente del Comité de Administración y Finanzas
+**[Universidad Panamericana] - Enero - Diciembre 2023**
+- Coordinación de un equipo de 20 estudiantes para llevar a cabo actividades para más de 600 estudiantes.
+- Organización de eventos, gestión de relaciones públicas y análisis de estados financieros para la toma de decisiones estratégicas.
+
+### Área de Operaciones 
+**[Inmobiliaria Magnum] - Enero 2020 - 2023.**
+- Atención al cliente y captación de nuevos prospectos.
+- Seguimiento y cierre de operaciones internas. 
+- Análisis de información financiera y control administrativo para la toma de decisiones.
+""")
+
+# Educación
+st.header("Educación")
+st.markdown("""
+**Licenciatura en Administración y Finanzas**
+- Universidad Panamericana - Campus Guadalajara - Agosto 2020 - Diciembre 2024
+
+### Otros cursos y certificaciones
+- Certificación SAP - Universidad Panamericana - Agosto 2023 - Diciembre 2023
+""")
+
+# Habilidades
+st.header("Habilidades")
+st.markdown("""
+- Toma de Desiciones
+- Trabajo en Equipo 
+- Solución de Problemas 
+- Liderazgo 
+""")
+
+# Idiomas
+st.header("Idiomas")
+st.markdown("""
+- Español - Nativo
+- Inglés - Avanzado
+""")
